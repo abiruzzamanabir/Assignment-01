ad0216f9feb45c04eb8a09e7634688ffb3ce383f
Abiruzzaman Abir <abiruzzamanabir17@gmail.com>
2022-03-17 13:00:32 +0600

    Namta Done

--- namta.php
+++ namta.php
@@ -6,4 +6,22 @@ for ($i=1; $i <=10 ; $i++) {
     $result= $input*$i;
     echo "{$input} X {$i} = {$result} <br>";
 }
+$input=3;
+echo "3 er namta<br>";
+for ($i=1; $i <=10 ; $i++) { 
+    $result= $input*$i;
+    echo "{$input} X {$i} = {$result} <br>";
+}
+$input=4;
+echo "4 er namta<br>";
+for ($i=1; $i <=10 ; $i++) { 
+    $result= $input*$i;
+    echo "{$input} X {$i} = {$result} <br>";
+}
+$input=5;
+echo "5 er namta<br>";
+for ($i=1; $i <=10 ; $i++) { 
+    $result= $input*$i;
+    echo "{$input} X {$i} = {$result} <br>";
+}
 ?>
